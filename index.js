9ac9952d7a100dce306a783d05c86473a15daf45
subhamManU <subhamsaha90@gmail.com>
2020-01-13 11:44:17 +0530

    All 24 languages added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.iframe-div audio').css('display', 'none');
     
-        for(var i=0;i<6;i++){
+        for(var i=0;i< 25;i++){
             var ele = $("."+i+"language");
             ele.css('display','none');
         }
@@ -16,7 +16,12 @@ function buttonClicked() {
     $(".button-row").hide();
     // $('.iframe-div audio').show();
     document.getElementById('audioplayer').play();
-    for (var i = 0; i <= 10 ; i++) {
+    showLanguages();
+}
+
+async function showLanguages(){
+    console.log('show()');
+    for (var i = 0; i < 25 ; i++) {
         window.setTimeout(
           (function (i){ 
             return function() {
@@ -24,5 +29,20 @@ function buttonClicked() {
                 ele.css('display','block');
             }
           })(i),i * 1000)
+          await sleep(2000);
         }
+        
+        clearLanguages();
 }
+ function clearLanguages(){
+     console.log('clear()');
+    for(var i=0;i< 25;i++){
+        var ele = $("."+i+"language");
+        ele.css('display','none');
+    }
+ }
+
+ function sleep(ms) {
+    console.log('sleep()');
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
